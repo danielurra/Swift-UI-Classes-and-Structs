d71af5ac1064606dca0a5f95f1a87aba186b8083
Daniel Urra <danielurra75@gmail.com>
2024-03-12 05:18:19 -0500

    Create classes-and-structs.swift

---
+++ classes-and-structs.swift
@@ -0,0 +1,48 @@
+// CLASS
+class Dog {
+    var name = ""
+    var age = 0
+    var furColor = ""
+    var whoownsit = ""
+}
+// create a particular instance of the above Class
+// my own doggy "Cacho"
+var cachoMyDog = Dog()
+cachoMyDog.age = 7
+cachoMyDog.furColor = "Brown"
+cachoMyDog.name = "Cacho"
+cachoMyDog.whoownsit = "Danny"
+
+// I can change values of each property
+cachoMyDog.age = 11
+// To print just type each property
+cachoMyDog.age
+cachoMyDog.furColor
+cachoMyDog.name
+cachoMyDog.whoownsit
+
+// *** CREAR ***
+// *** CREAR *** otra particular instances of the above Class del perro de mi vecina la Sra. Betty
+var myNeighborDog = Dog()
+myNeighborDog.age = 3
+myNeighborDog.furColor = "White"
+myNeighborDog.name = "Pluto"
+myNeighborDog.whoownsit = "Betty"
+
+// *** MOSTRAR ***
+// *** MOSTRAR *** los valore asignados a ese perro en particular
+myNeighborDog.age
+myNeighborDog.furColor
+myNeighborDog.name
+myNeighborDog.whoownsit
+
+
+
+
+
+
+
+
+
+
+
